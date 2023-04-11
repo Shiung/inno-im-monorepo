712aa185109b99c3512b34bc6ac737dc86b644e0
blesscat <blesscat@gmail.com>
2023-04-11 18:14:47 +0800

    [mockServer] im archor

---
+++ apps/mockServer/src/mock/im/index.ts
@@ -0,0 +1,48 @@
+import { mock, Random } from 'mockjs'
+
+import type { IMockData } from '../../types'
+
+Random.extend({
+  housename: function(date) {
+    console.log('random extend date', date)
+    const names = ['球球直播', '娟娟直播', '糖果直播', '君君直播']
+    return this.pick(names)
+  },
+  nickname: function() {
+    const names = ['翠花姑凉的小花', '娟娟', '糖果', '君君']
+    return this.pick(names)
+  }
+})
+
+export default [
+  {
+    url: '/anchor/web-anchors',
+    timeout: 500,
+    response: ({ params }) => mock({
+      "code": "0",
+      "data": {
+          "list|2-20": [
+            {
+              "attentionStatus": 0,
+              "fansCount": () => Random.natural(0, 10000000),
+              "houseId": () => String(Random.natural(0, 99999)),
+              "houseImage": "https://oss-logo-hk.oss-accelerate.aliyuncs.com/business/image/596/PztnHi1kR12iCdGOCr1lvA.png",
+              "houseName": "@HOUSENAME",
+              "liveStatus": () => Random.natural(1, 4),
+              "nickName": "@NICKNAME",
+              "playStreamAddress": "https://live5.hqzhuce.com/live/10596.flv?auth_key=1681107440-0-596-0c34337853d9ae4d8bd536ab2ea083da",
+              "userImage": "https://oss-logo-hk.oss-accelerate.aliyuncs.com/business/image/596/4Hl7wS2hSoOb-brzYS1yLw.jpg",
+              "visitHistory": () => Random.natural(0, 999999999999)
+            }
+          ],
+          "pager": {
+              "pageIdx": params['pageIdx'],
+              "pageSize": params['pageSize'],
+              "totalPage": 1,
+              "totalRow": 2
+          }
+      },
+      "message": "",
+    })
+  }
+] as IMockData[]
--- apps/mockServer/src/types.ts
+++ apps/mockServer/src/types.ts
@@ -1,8 +1,10 @@
+import type { Request } from 'express'
+
 export type IMethod = 'get' | 'post' | 'put' | 'patch' | 'delete'
 
 export interface IMockData {
   url: string
   method?: IMethod
   timeout?: number
-  response?: () => any
+  response?: (req: Request) => any
 }
