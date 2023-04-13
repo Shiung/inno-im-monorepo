75666936b1ac6cb261e3bae69f231af339a05d20
blesscat <blesscat@gmail.com>
2023-04-13 13:59:36 +0800

    [testing] convertSid test.

--- packages/utils/convertSid/convertSid.test.ts
+++ packages/utils/convertSid/convertSid.test.ts
@@ -1,8 +1,27 @@
 import '@testing-library/jest-dom'
 import convertSid from './index'
 
-test('testttt', () => {
-  const t = convertSid('1')
+import { SID } from '../types'
 
-  expect(t).toBe(1)
+test('convert to ball sid', () => {
+  const all = convertSid('all')
+  expect(all).toBe(SID.all)
+
+  const soccer = convertSid('1')
+  expect(soccer).toBe(SID.soccer)
+
+  const basketball = convertSid('2')
+  expect(basketball).toBe(SID.basketBall)
+
+  const tennis = convertSid('3')
+  expect(tennis).toBe(SID.tennis)
+
+  const baseball = convertSid('4')
+  expect(baseball).toBe(SID.baseBall)
+})
+
+test('Not All and Not Number is going to convert to NaN', () => {
+  const sid = convertSid('string')
+
+  expect(Number.isNaN(sid)).toBe(true)
 })
