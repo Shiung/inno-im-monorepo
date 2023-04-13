180b2f1ce5667ba1d72b72ac614cc437842f929d
FE-Jack <jacks@innotech.me>
2023-04-13 11:12:36 +0800

    fix fold name create useful name

---
+++ apps/library/src/containers/HeaderNavigation/index.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+import BallSelection from './BallSelection/index.svelte'
+import BonusPoint from './BonusPoint/index.svelte'
+</script>
+
+<div>
+  <div class='flex fixed w-full px-[8px] justify-between items-center h-[44px] im-shadow bg-white z-10'>
+    <BallSelection />
+    <BonusPoint />
+  </div>
+
+  <div class='h-[44px]'/>
+</div>
