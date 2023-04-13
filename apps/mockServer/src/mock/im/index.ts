3cbc2c8a6a7e8947ac305ef7962f5eda09ace7b4
blesscat <blesscat@gmail.com>
2023-04-13 18:01:48 +0800

    [api] 更新 mock data

--- apps/mockServer/src/mock/im/index.ts
+++ apps/mockServer/src/mock/im/index.ts
@@ -25,23 +25,22 @@ export default [
   },
   // #5.專家預測
   {
-    url: 'expert/predictions',
+    url: '/expert/predictions',
     response: () => mock({
       "message": "",
       "code": "0",
       "data": {
-        "list:":[
+        "list|5-20":[
           {
-              "expertId": "@paragraph",
+              "expertId": "@word",
               "expertName": "@name",
-              // "expertImage": "http://file.fengkuangtiyu.cn/imagesER/010/01016777388541379038.png",
-              "expertImage": "@image",
+              "expertImage": "http://file.fengkuangtiyu.cn/imagesER/010/01016777388541379038.png",
               "releaseTime": 1667504351531,
               "closeTime": 1667504371531,
               "market": "獨贏",
               "hotStreak": Random.integer(0, 12),
               "hitRate": Random.integer(60, 100),
-              "articleId": "wv78xixr24nhokr",
+              "articleId": "@word",
               "articleStatus": Random.integer(1, 2),
               "title": "@ctitle",
               "homeName": "@cname",
