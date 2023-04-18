722892f28e027d5642dfea776f2a14dc73d785ca
blesscat <blesscat@gmail.com>
2023-04-18 16:23:14 +0800

    [mockServer] IM_BE_URL -> IM_API_URL

--- apps/mockServer/src/moduleDict.ts
+++ apps/mockServer/src/moduleDict.ts
@@ -1,6 +1,6 @@
 const moduleDict: { [apiUrl: string]: () => Promise<any>} = {
   ECHO_URL: () => import('./mock/echo'),
-  IM_BE_URL: () => import('./mock/im')
+  IM_API_URL: () => import('./mock/im')
 }
 
 export default moduleDict
