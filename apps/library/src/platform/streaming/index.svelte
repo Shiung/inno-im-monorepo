98946d3692f1cf47ee6cccce857ce8761c526820
blesscat <blesscat@gmail.com>
2023-04-25 16:58:53 +0800

    [platform] streaming 修正測試資料

--- apps/library/src/platform/streaming/index.svelte
+++ apps/library/src/platform/streaming/index.svelte
@@ -5,7 +5,7 @@ $: streaming = anchor.streaming
 </script>
 
 <div class='h-[214px]'>
-  <div> {$streaming.houseId} </div>
-  <div> {$streaming.nickName} </div>
-  <div> {$streaming.houseName} </div>
+  <div> {$streaming?.houseId} </div>
+  <div> {$streaming?.nickName} </div>
+  <div> {$streaming?.houseName} </div>
 </div>
