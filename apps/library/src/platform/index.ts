2549722ae1486721e5f38ed78ca44c191cbc1949
blesscat <blesscat@gmail.com>
2023-04-26 18:44:07 +0800

    [library] 總進入點

---
+++ apps/library/src/platform/index.ts
@@ -0,0 +1,17 @@
+import './library.css'
+import '../commonInit'
+
+// Reexport your entry components here
+export { default as App } from '../App.svelte'
+export { default as Anchor } from './anchors'
+export { default as Streaming } from './streaming'
+// export * as store from './platform/store'
+
+// deprecated, 下面幾個已棄用，待平台更新後替換掉
+export { default as PlatformExpert } from '$pages/platformExpert'
+export * from '$pages/platformExpert'
+// export * as store from '$stores/library'
+
+// 下面是測試用的 等正式上版到prod後要刪除
+export { default as Test } from '$pages/test/index.svelte'                                                                                                                                    
+export { default as Uikit } from '$pages/test/index.svelte'
