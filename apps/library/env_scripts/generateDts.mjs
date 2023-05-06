22e29248294c2c6078c76a22c61d5812eed2a4e3
Sean Zhengg <s410585038@gmail.com>
2023-05-07 01:49:09 +0800

    [dts2]: complete

--- apps/library/env_scripts/generateDts.mjs
+++ apps/library/env_scripts/generateDts.mjs
@@ -2,6 +2,7 @@ import { emitDts } from 'svelte2tsx'
 import fs from 'fs'
 import path from 'path'
 import ts from 'typescript'
+import { createRequire } from 'node:module'
 
 // ----- PLAN A -----
 // fs.rmSync('types', { recursive: true, force: true })
@@ -10,38 +11,38 @@ import ts from 'typescript'
 // const { error, config: tsConfig } = ts.readConfigFile('tsconfig.json', ts.sys.readFile)
 // if (error) await $`exit 1`
 
-// delete tsConfig.references
-// tsConfig.include = ['src/platform/**/*']
-// tsConfig.exclude = ['src/platform/**/*.test.ts','src/platform/**/*.test.d.ts']
-// tsConfig.compilerOptions.paths = {
-//   ...tsConfig.compilerOptions.paths,
-//   "utils/*": ["../../packages/utils/*"],
-//   "ui/*": ["../../packages/ui/*"]
-// }
+// tsConfig.include = tsConfig.include.map(e => e.replace(/src/, 'src/platform'))
+// tsConfig.exclude = ['**/*.test.ts']
 
 // fs.writeFileSync('tsconfig.json', JSON.stringify(tsConfig, null, 2))
 
+// const require = createRequire(import.meta.url)
+
 // await emitDts({
 //   declarationDir: 'types',
-//   svelteShimsPath: 'svelte2tsx/svelte-shims.d.ts'
+//   svelteShimsPath: require.resolve('svelte2tsx/svelte-shims.d.ts')
 // })
 
 // fs.writeFileSync('tsconfig.json', originTsConfigContents)
 
 // ----- PLAN B -----
+fs.rmSync('types', { recursive: true, force: true })
+
 const absPath = path.join(process.cwd(), 'src/platform')
 const { error, config: tsConfig } = ts.readConfigFile('tsconfig.json', ts.sys.readFile)
 if (error) await $`exit 1`
 
-delete tsConfig.references
 tsConfig.include = tsConfig.include.map(e => e.replace(/src/, '.'))
 tsConfig.exclude = ['./**/*.test.ts']
-tsConfig.compilerOptions.paths = { ...tsConfig.compilerOptions.paths, "utils/*": ['../../packages/utils/*'], "ui/*": ['../../packages/ui/*'] }
 
 fs.writeFileSync(absPath + '/tsconfig.json', JSON.stringify(tsConfig, null, 2))
+
+const require = createRequire(import.meta.url)
+
 await emitDts({
   libRoot: absPath,
   declarationDir: '../types',
-  svelteShimsPath: 'svelte2tsx/svelte-shims.d.ts'
+  svelteShimsPath: require.resolve('svelte2tsx/svelte-shims.d.ts')
 })
+
 fs.rmSync(absPath + '/tsconfig.json')
