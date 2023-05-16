d2e7cab8610ed5430f124c37c052a92ecd52949a
FE3-Sean <sean@innotech.me>
2023-05-16 15:28:05 +0800

    [GDIM-53][ArticleStory]: update Loading comp

--- apps/library/src/pages/expertDetail/Ongoing/components/ArticleStory/components/Loading/index.svelte
+++ apps/library/src/pages/expertDetail/Ongoing/components/ArticleStory/components/Loading/index.svelte
@@ -1,1 +1,3 @@
-<div class='h-[84px] animate-pulse bg-[#eee] rounded-[10px]'></div>
+<div class='bg-white px-3 pt-3 pb-2'>
+  <div class='h-[84px] relative z-[1] animate-pulse bg-[#eee] rounded-[10px]'></div>
+</div>
