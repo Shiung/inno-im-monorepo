4b48e2c278af51c145a0b98118caacd52f3f5bbd
FE3-Sean <sean@innotech.me>
2023-05-18 17:47:45 +0800

    [GDIM-53][BonusPoint]: add amountThousandthTransformer

--- apps/library/src/containers/HeaderNavigation/BonusPoint/index.svelte
+++ apps/library/src/containers/HeaderNavigation/BonusPoint/index.svelte
@@ -1,5 +1,7 @@
 <script lang='ts'>
 import bonusPoint from '../images/bonusPoint.webp'
+import { amountThousandthTransformer } from 'utils/amount'
+
 export let bonus: number = 0
 
 </script>
@@ -7,5 +9,5 @@ export let bonus: number = 0
 <div class='relative flex items-center h-[20px] rounded-[15px] overflow-hidden px-[6px]'>
   <div class='absolute inset-0 bg-imprimary z-[-1] opacity-10' />
   <img class='w-[12px] h-[12px] mr-[6px]' src={bonusPoint} alt='' />
-  <div class='text-[#333333] text-[11px]'> {bonus} </div>
+  <div class='text-[#333333] text-[11px]'> {amountThousandthTransformer(bonus)} </div>
 </div>
