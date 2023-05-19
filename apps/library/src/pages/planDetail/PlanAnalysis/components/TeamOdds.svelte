f1373b9c354ccd1478e235257ff1eaa963324755
FE3-Sean <sean@innotech.me>
2023-05-19 16:27:48 +0800

    [GDIM-53][PlanAnalysis]: unlocked content & active status for TeamOdds

--- apps/library/src/pages/planDetail/PlanAnalysis/components/TeamOdds.svelte
+++ apps/library/src/pages/planDetail/PlanAnalysis/components/TeamOdds.svelte
@@ -2,7 +2,9 @@
   import type { IArticleDetail } from 'api/im/types'
   import { twMerge } from 'tailwind-merge';
   import { beImgUrlParse, ImageType } from 'utils/imgUrlParse'
+  import { t } from '$src/stores';
 
+  import { Badget } from 'ui';
   import TeamLogo from '$src/components/TeamLogo'
 
   export let data: IArticleDetail
@@ -13,33 +15,68 @@
   $: info = {
     name: type === 'home' ? data.homeName : data.awayName,
     id: type === 'home' ? data.homeId : data.awayId,
-    ...getOddAndK()
+    ...marketTypeDispatcher(data, type)(getKAndOdds)
   }
+  
+  $: active = marketTypeDispatcher(data, type)(getMatchResult)
 
-  const getOddAndK = (): { k: string; odd: string } => {
-    function _mlAndAhResult() {
+  const getKAndOdds = {
+    ml(data, type) {
+      if (type === 'home') return { k: data?.homeName, odd: data?.odds?.[0]?.h }
+      else return { k: data?.awayName, odd: data?.odds?.[0]?.a }
+    },
+    ah(data, type) {
       if (type === 'home') return { k: data?.odds?.[0]?.k, odd: data?.odds?.[0]?.h }
       else return { k: data?.odds?.[0]?.k, odd: data?.odds?.[0]?.a }
-    }
-    function _ouResult() {
+    },
+    ou(data, type) {
       if (type === 'home') return { k: data?.odds?.[0]?.k, odd: data?.odds?.[0]?.ov }
       else return { k: data?.odds?.[0]?.k, odd: data?.odds?.[0]?.ud }
-    }
-
-    switch (data.marketType) {
-      case '11':
-      case '12':
-      case '21':
-      case '22':
-        return _mlAndAhResult()
-      case '13':
-      case '23':
-        return _ouResult()
+    },
+  }
+  const getMatchResult = {
+    ml(data, type) {
+      if (type === 'home') return data?.matchResult === 'h'
+      else return data?.matchResult === 'a'
+    },
+    ah(data, type) {
+      if (type === 'home') return data?.matchResult === 'h'
+      else return data?.matchResult === 'a'
+    },
+    ou(data, type) {
+      if (type === 'home') return data?.matchResult === 'ov'
+      else return data?.matchResult === 'ud'
+    },
+  }
+  const marketTypeDispatcher = (data, type) => {
+    return (resolver) => {
+      switch (data?.marketType) {
+        case '11':
+        case '21':
+        return resolver['ml'](data, type)
+        case '12':
+        case '22':
+          return resolver['ah'](data, type)
+        case '13':
+        case '23':
+          return resolver['ou'](data, type)
+      }
     }
   }
 </script>
 
-<div class={twMerge('flex flex-col items-center bg-white w-full shadow-[0_2px_6px_0_rgba(0,0,0,0.1)] rounded-[10px] py-2', $$props.class)}>
+<div
+  class={twMerge(
+    'flex flex-col items-center bg-white w-full shadow-[0_2px_6px_0_rgba(0,0,0,0.1)] rounded-[10px] py-2',
+    active ? 'relative border border-[rgba(var(--im-monorepo-primary))]' : '',
+    $$props.class
+  )}
+>
+  {#if active}
+    <div class='absolute -top-[1px] -left-[1px]'>
+      <Badget class='flex'> { $t('common.recommend') } </Badget>
+    </div>
+  {/if}
   <div class="text-center">
     <TeamLogo class='w-[30px] h-[30px]' src={beImgUrlParse({ id: info.id, type: ImageType.competitors })}/>
     <span class='text-[12px] leading-[18px]'> {info.name} </span>
--- apps/library/src/pages/planDetail/PlanAnalysis/index.svelte
+++ apps/library/src/pages/planDetail/PlanAnalysis/index.svelte
@@ -1,26 +1,37 @@
 <script lang="ts">
-  import type { IArticleDetail } from 'api/im/types'
   import { t } from '$stores'
-  import Lock from './images/lock.svg'
+  import type { IArticleDetail } from 'api/im/types'
+  
   import TeamOdds from './components/TeamOdds.svelte'
+  
+  import Lock from './images/lock.svg'
+
   export let data: IArticleDetail
+  let unlocked: boolean = false
 </script>
 
 <div class="bg-transparent px-3 pb-3">
-  <div class="flex flex-col items-center rounded-[10px] bg-[#f7f8f9] px-3 pt-2 space-y-5">
+  <div class="flex flex-col items-center rounded-[10px] bg-[#f7f8f9] px-3 pt-2">
     <div class="w-full flex space-x-2">
       <TeamOdds data={data} type='home' class='flex-1' />
 
       <TeamOdds data={data} type='away' class='flex-1' />
     </div>
 
-    <div class="text-center">
-      <p class="text-sm font-semibold text-[#333]">
-        <Lock class='inline-block align-middle' width={20} height={20} />
-        <span class='inline-block align-middle'> {$t('expert.planDetail.planStillLocked')} </span>
-      </p>
+    {#if unlocked}
+      <div class="text-center mt-5">
+        <p class="text-sm font-semibold text-[#333]">
+          <Lock class='inline-block align-middle' width={20} height={20} />
+          <span class='inline-block align-middle'> {$t('expert.planDetail.planStillLocked')} </span>
+        </p>
 
-      <span class='inline-block text-[12px] leading-[18px] text-[#666] mb-[30px]'> {$t('expert.planDetail.unlockDescription')} </span>
-    </div>
+        <span class='inline-block text-[12px] leading-[18px] text-[#666] mb-[30px]'> {$t('expert.planDetail.unlockDescription')} </span>
+      </div>
+    {:else}
+      <div class="mt-3">
+        <span class='inline-block text-[12px] leading-[21px] text-[#666] mb-[12px]'> {data?.content} </span>
+      </div>
+    {/if}
+
   </div>
 </div>
