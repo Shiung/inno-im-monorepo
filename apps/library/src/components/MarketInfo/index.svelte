9084e94ae61046e08f0dc4cd1f7b436b7c9d346a
FE3-Sean <sean@innotech.me>
2023-05-19 17:33:34 +0800

    [GDIM-53][MarketInfo]: remove unused code

--- apps/library/src/components/MarketInfo/index.svelte
+++ apps/library/src/components/MarketInfo/index.svelte
@@ -6,34 +6,6 @@ import MarketBadget from '$src/components/MarketBadget'
 import { marketTypeDispatcher } from '$src/utils/match'
 export let article: IArticle
 
-const mlAndAhResult = (_result: IArticle['matchResult']): string => {
-  if (_result === 'h') return `${article.homeName} ${article?.odds?.[0]?.h ? '@' + article?.odds?.[0]?.h : ''}`
-  else if (_result === 'a') return `${article.awayName} ${article?.odds?.[0]?.a ? '@' + article?.odds?.[0]?.a : ''}`
-  else return ''
-}
-
-const ouResult = (_result: IArticle['matchResult']): string => {
-  if (_result === 'ov') return `${$t('common.over')} ${article?.odds?.[0]?.ov ? '@' + article?.odds?.[0]?.ov : ''}`
-  else if (_result === 'ud') return `${$t('common.under')} ${article?.odds?.[0]?.ud ? '@' + article?.odds?.[0]?.ud : ''}`
-  else return ''
-}
-
-
-const betTypeGenerator = (article?: IArticle) => {
-  if (!article) return ''
-
-  switch (article.marketType) {
-    case '11':
-    case '21':
-    case '12':
-    case '22':
-      return mlAndAhResult(article.matchResult)
-    case '13':
-    case '23':
-      return ouResult(article.matchResult)
-  }
-}
-
 const getBetType = {
   ml(data) {
     if (data?.matchResult === 'h') return `${data?.homeName} ${data?.odds?.[0]?.h ? '@' + data?.odds?.[0]?.h : ''}`
