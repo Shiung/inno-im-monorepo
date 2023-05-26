b22f13b85d4fc2815e5c375429ec0b2a558d7944
FE3-Sean <sean@innotech.me>
2023-05-26 15:54:36 +0800

    [square][StreamBlock]: refactor

--- apps/library/src/pages/square/index.svelte
+++ apps/library/src/pages/square/index.svelte
@@ -1,21 +1,18 @@
 <script lang='ts'>
-  import FlvPlayer, { onError, onReady } from 'ui/components/FlvPlayer'
   import { params, replace } from 'svelte-spa-router'
   import type { IWebAnchor } from 'api/im/types'
-  import Circle from 'ui/core/button/loading.svelte'
-
+  
   import HeaderNavigation from '$containers/HeaderNavigation'
-  import HouseImage from '$src/components/HouseImage/index.svelte'
 
+  import StreamBlock from './StreamBlock'
   import AnchorBlock from './AnchorBlock'
   import ExpertBlock from './ExpertBlock'
 
   import { convertSid, type SidType } from 'utils'
 
+  let streaming: IWebAnchor
+
   $: sid = convertSid($params?.sid)
-  let streaming
-  let loading = false
-  let error = false
 
   const headNavIcons: { sid: SidType, onClick: () => void }[] = [
     {
@@ -35,43 +32,13 @@
   const onChange = (e: CustomEvent<IWebAnchor>) => {
     streaming = e.detail
   }
-
-  onError((e) => {
-    loading = false
-    error = true
-  })
-
-  onReady(() => {
-    loading = false
-  })
-
-  $: if(streaming) { error = false }
-  $: if(streaming && streaming?.liveStatus === 2) loading = true
 </script>
 
 <div>
   <HeaderNavigation active={sid} icons={headNavIcons} />
 
   <div class='space-y-[10px]'>
-    {#if streaming}
-      {#if streaming?.liveStatus === 2}
-        <!-- {#if !error} -->
-        <div class='relative min-h-[200px]'>
-          {#if loading}
-            <div class='absolute inset-0 bg-white flex items-center justify-center'>
-              <div class='w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[30px] overflow-hidden' >
-                <Circle stroke='rgb(var(--im-monorepo-primary))' />
-              </div>
-            </div>
-          {/if}
-
-          <FlvPlayer url={streaming?.playStreamAddress} />
-        </div>
-        <!-- {/if} -->
-      {:else}
-        <HouseImage src={streaming?.houseImage} />
-      {/if}
-    {/if}
+    <StreamBlock {streaming} />
 
     <AnchorBlock on:change={onChange} />
     {#if sid === 1 || sid === 2}
