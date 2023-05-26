f56ef02ab9c6fe8f558758ed61912874f6ffe75c
blesscat <blesscat@gmail.com>
2023-05-26 13:24:46 +0800

    [planDetail] using im-shadow instead shadow generator.

--- apps/library/src/pages/planDetail/BottomPanel/index.svelte
+++ apps/library/src/pages/planDetail/BottomPanel/index.svelte
@@ -5,7 +5,7 @@
 
 <div data-cid="DetailBottomPanel">
   <div bind:this={dom} class="fixed z-10 bottom-0 left-0 right-0">
-    <div class="p-4 rounded-t-[20px] shadow-[0_0_10px_0_rgba(0,0,0,0.1)] bg-white">
+    <div class="p-4 rounded-t-[20px] im-shadow bg-white">
       <slot />
     </div>
   </div>
