7b706efa36491d7caa3602af210f41d272bf3f9c
FE-David <davidj@innotech.me>
2023-05-26 17:48:49 +0800

    新增 test

---
+++ apps/library/src/pages/test/expert/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { push } from 'svelte-spa-router'
+
+  import List, { setSid, goToDetail } from '$src/platform/expert'
+
+  onMount(()=>{
+    setSid("1")
+    setTimeout(() => {
+      goToDetail(() => (window.location.href = '/expertDetail/123/plan'))
+    }, 1000)
+  })
+
+</script>
+
+<List />
