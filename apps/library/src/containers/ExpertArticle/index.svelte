0589516657c320b2feb348e5e1c46a31c84cdc72
blesscat <blesscat@gmail.com>
2023-06-09 13:57:48 +0800

    merge

--- apps/library/src/containers/ExpertArticle/index.svelte
+++ apps/library/src/containers/ExpertArticle/index.svelte
@@ -6,18 +6,19 @@
   import { locale } from '$stores'
 
   import MarketInfo from '$src/components/MarketInfo'
-  
+
   import type { IArticle } from 'api/im/types/expert'
   import cnWinStamp from './images/zh_CN/ic_stamp_win.webp'
   import enWinStamp from './images/en_US/ic_stamp_win.webp'
-  
+
   export let article: IArticle
 
   export let hideStamp: boolean = false
-  
+
   const getStamp = (hitStatus: IArticle['hitStatus']) => {
     switch (hitStatus) {
-      case 1: return ['zh_CN', 'zh_HK'].includes($locale) ? cnWinStamp : enWinStamp
+      case 1:
+        return ['zh_CN', 'zh_HK'].includes($locale) ? cnWinStamp : enWinStamp
       case 2:
       default:
         return ''
@@ -28,26 +29,34 @@
     push(`/planDetail/${$params.expertId}/${article?.articleId}`)
   }
 
-  $: stampStyle = hideStamp ? null : `background-image: url(${getStamp(article.hitStatus)});background-position: right -13px top -15px; background-size: 72px`
+  $: stampStyle = hideStamp
+    ? null
+    : `background-image: url(${getStamp(
+        article.hitStatus
+      )});background-position: right -13px top -15px; background-size: 72px`
 </script>
 
-<Ripple class={twMerge('w-full rounded-[10px] im-shadow bg-no-repeat p-[8px]', $$props.class)} style={stampStyle} on:click={onClick}>
-  <div class='text-left text-[14px] font-semibold'>
-    {timestampToFormat({ ts: article.releaseTime, format: 'MM-DD' }) } </div>
+<Ripple
+  class={twMerge('w-full rounded-[10px] im-shadow bg-no-repeat p-[8px]', $$props.class)}
+  style={stampStyle}
+  on:click={onClick}
+>
+  <div class="text-[14px] font-semibold">
+    {timestampToFormat({ ts: article.releaseTime, format: 'MM-DD' })}
+  </div>
 
-  <div class='flex items-center justify-between mt-[8px]'>
-    <div class='text-[#999999] text-[12px]'> {article.tnName} </div>
-    <div class='text-[12px]'>
+  <div class="flex items-center justify-between mt-[8px]">
+    <div class="text-[#999999] text-[12px]">{article.tnName}</div>
+    <div class="text-[12px]">
       <span> {article.homeName} </span>
       <span> vs </span>
       <span> {article.awayName} </span>
     </div>
   </div>
 
-  <div class='bg-[#eeeeee] h-[1px] my-[8px]' />
+  <div class="bg-[#eeeeee] h-[1px] my-[8px]" />
 
-  <div class='truncate text-[14px] font-semibold'> {article.title} </div>
+  <div class="truncate text-[14px] font-semibold">{article.title}</div>
 
-  <MarketInfo class='mt-[8px]' article={article} />
-
+  <MarketInfo class="mt-[8px]" {article} />
 </Ripple>
