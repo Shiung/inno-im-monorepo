040988fe51a1f88cac260ac68b296951a32d1d9a
FE-David <davidj@innotech.me>
2023-06-12 03:15:12 +0800

    新增 CurrencyIcon 元件

---
+++ apps/library/src/components/CurrencyIcon/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let currency: string
+  export let size: string
+  const src = new URL(`./images/${currency}.png`, import.meta.url).pathname
+</script>
+
+<img class={`w-[${size}] h-[${size}]`} {src} alt="" />
