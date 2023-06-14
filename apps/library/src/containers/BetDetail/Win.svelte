2a24b8af59cbe6de066973338731552916a56eec
FE-David <davidj@innotech.me>
2023-06-14 11:58:11 +0800

    曬單 修正 Win 元件

--- apps/library/src/containers/BetDetail/Win.svelte
+++ apps/library/src/containers/BetDetail/Win.svelte
@@ -6,13 +6,25 @@
 
   export let betItem
 
-  const { currency } = betItem
+  const { currency, details, cashOut, mayWinAmount, netWin } = betItem
+  const settle = details[0].settle !== 0 || cashOut
 </script>
 
-<div class="flex items-center">
-  <div class="text-[#999]">{$t('chat.toWin')}</div>
+<div class="flex items-center font-semibold">
+  {#if settle}
+    <CurrencyIcon class="w-[15px] h-[15px] mr-[5px]" {currency} />
 
-  <CurrencyIcon class="w-[20px] h-[20px] mx-[5px]" {currency} />
+    <div class="text-[18px] text-[#cb0202]">
+      {amountThousandthTransformer(netWin, {
+        format: { prefix: netWin > 0 ? '+' : '' },
+        decimal: 0
+      })}
+    </div>
+  {:else}
+    <div class="text-[#999]">{$t('chat.toWin')}</div>
 
-  <div class="text-[18px]">{amountThousandthTransformer(betItem.mayWinAmount)}</div>
+    <CurrencyIcon class="w-[20px] h-[20px] mx-[5px]" {currency} />
+
+    <div class="text-[18px]">{amountThousandthTransformer(mayWinAmount)}</div>
+  {/if}
 </div>
