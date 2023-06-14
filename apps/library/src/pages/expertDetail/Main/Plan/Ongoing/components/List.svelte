ebe77a6bfb1ecbd93eaf1918ab03ee2eea89d431
FE3-Sean <sean@innotech.me>
2023-06-14 13:35:53 +0800

    [Ongoing][List]: use local const template to retrieve value

--- apps/library/src/pages/expertDetail/Main/Plan/Ongoing/components/List.svelte
+++ apps/library/src/pages/expertDetail/Main/Plan/Ongoing/components/List.svelte
@@ -12,17 +12,18 @@
 <div class='space-y-[12px]'>
 
   {#each articles as article}
+    {@const {homeName, awayName, title, articleId } = article || {}}
     <div class='space-y-[8px] im-shadow rounded-[10px] p-[8px]'>
-      <div class='truncate'> {article.title} </div>
+      <div class='truncate'> {title} </div>
       <!-- 玩法區塊 待討論 -->
       <div class='flex items-center'>
         <MarkInfo article={article} />
-        {#if article?.homeName && article?.awayName}
-          <div class='text-[12px] ml-[4px]'> {`${article.homeName} vs ${article.awayName}`} </div>
+        {#if homeName && awayName}
+          <div class='text-[12px] ml-[4px]'> {`${homeName} vs ${awayName}`} </div>
         {/if}
       </div>
 
-      <Button class='w-full rounded-[12px] h-[44px]' on:click={() => push(`/planDetail/${$params.expertId}/${article.articleId}`)}>
+      <Button class='w-full rounded-[12px] h-[44px]' on:click={() => push(`/planDetail/${$params.expertId}/${articleId}`)}>
         {$t('expert.plan.prediction')}
       </Button>
     </div>
