d29ac7db94c71d1e8f7efce72ff5473601b5641f
FE3-Sean <sean@innotech.me>
2023-06-14 17:01:44 +0800

    [Life]: add Empty

--- apps/library/src/containers/AnchorDetailSheet/Life/index.svelte
+++ apps/library/src/containers/AnchorDetailSheet/Life/index.svelte
@@ -2,6 +2,7 @@
 import { timestampToFormat } from 'utils/convertDateAndTimestamp'
 import { t } from '$stores'
 import Loading from './Loading.svelte'
+import Empty from '$containers/Empty'
 
 import type { IWebAnchorLife } from 'api/im/types'
 
@@ -34,22 +35,27 @@ const parseLifeData = (list?: typeof life.data.list) => {
   {#if life.loading}
     <Loading />
   {:else}
-    {#each parseLifeData(life?.data?.list) as [year, stories]}
-      <div class='font-semibold text-[18px]'> {$t('common.year', { num: year })} </div>
+    {@const list = life?.data?.list || []}
+      {#if list.length === 0}
+        <Empty class='h-[200px]'/>
+      {:else}
+        {#each parseLifeData(list) as [year, stories]}
+          <div class='font-semibold text-[18px]'> {$t('common.year', { num: year })} </div>
 
-      <div class='space-y-[20px] mt-[16px]'>
-        {#each stories as story}
-          <div class='grid grid-cols-[45px_80px_auto] gap-[15px] h-[80px] overflow-y-hidden'>
-            <div>
-              <div class='text-[14px]'> {story.day} </div>
-              <div class='text-[10px] text-[#999999]'> {story.time} </div>
-            </div>
-            <img class='w-[80px] h-[80px]' src={story.image} alt='' />
-            <div class='text-[12px] h-full'> {story.context} </div>
+          <div class='space-y-[20px] my-[16px]'>
+            {#each stories as story}
+              <div class='grid grid-cols-[45px_80px_auto] gap-[15px]'>
+                <div>
+                  <div class='text-[14px]'> {story.day} </div>
+                  <div class='text-[10px] text-[#999999]'> {story.time} </div>
+                </div>
+                <img class='w-[80px] h-[80px]' src={story.image} alt='' />
+                <div class='text-[12px] h-full'> {story.context} </div>
+              </div>
+            {/each}
           </div>
-        {/each}
-      </div>
 
-    {/each}
+        {/each}
+      {/if}
   {/if}
 </div>
