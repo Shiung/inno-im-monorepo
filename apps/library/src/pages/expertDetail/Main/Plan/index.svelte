c062bee520d6e60bce6e18c5da09f3ec4a01da9d
FE3-Sean <sean@innotech.me>
2023-06-19 17:31:10 +0800

    [expertDetail][Plan]: remove test code

--- apps/library/src/pages/expertDetail/Main/Plan/index.svelte
+++ apps/library/src/pages/expertDetail/Main/Plan/index.svelte
@@ -2,9 +2,6 @@
 import Ongoing from './Ongoing/index.svelte'
 import History from './History/index.svelte'
 import { locale } from '$stores'
-setTimeout(() => {
-  locale.set('en_US')
-}, 3000)
 </script>
 
 <div data-cid='expertDetail_Main_Plan' class='space-y-[25px] mx-[16px]'>
