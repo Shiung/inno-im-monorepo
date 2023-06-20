5ed2a6c2f91d4269a51b4d4f9255839fe6517ddd
FE3-Sean <sean@innotech.me>
2023-06-20 11:40:53 +0800

    [viteConfig][env]: use .env file variables to build library package into platform project

--- apps/library/vite.config.ts
+++ apps/library/vite.config.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import svelteSVG from 'vite-plugin-svelte-svg'
 import { execSync } from 'child_process'
@@ -11,51 +11,69 @@ const svelteSVGPlugin = () => svelteSVG({
   requireSuffix: false
 })
 
-const buildAsLibrary = () => ({
-  build: {
-    target: ['chrome70'],
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: {
-        index: resolve(__dirname, './src/platform/index.ts'),
-        app: resolve(__dirname, './src/platform/app.ts'),
-        anchors: resolve(__dirname, './src/platform/anchors/index.ts'),
-        streaming: resolve(__dirname, './src/platform/streaming/index.ts'),
-        chartroom: resolve(__dirname, './src/platform/chatroom/index.ts'),
-        expert: resolve(__dirname, './src/platform/expert/index.ts')        
-      },
-      name: 'im-library',
-      // the proper extensions will be added
-      fileName: (format: string, entryName: string) => {
-        if (format === 'es') return `${entryName}.js`
-        if (format === 'cjs') return `${entryName}.cjs`
-      }
-    }
+const buildToPlatformNodeModules = () => {
+  if(!process?.env?.PLATFORM_OUT_DIR) {
+    console.warn("\x1b[33m[WARNING] buildToPlatformNodeModules miss PLATFORM_OUT_DIR variable which is defined in .env file, use default setting.\n\x1b[0m")
+    return null
   }
-})
+  return {
+    outDir: process.env.PLATFORM_OUT_DIR || 'dist',
+    emptyOutDir: true
+  }
+}
+
+const buildAsLibrary = () => {
+  return ({
+    build: {
+      target: ['chrome70'],
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: {
+          index: resolve(__dirname, './src/platform/index.ts'),
+          app: resolve(__dirname, './src/platform/app.ts'),
+          anchors: resolve(__dirname, './src/platform/anchors/index.ts'),
+          streaming: resolve(__dirname, './src/platform/streaming/index.ts'),
+          chartroom: resolve(__dirname, './src/platform/chatroom/index.ts'),
+          expert: resolve(__dirname, './src/platform/expert/index.ts')        
+        },
+        name: 'im-library',
+        // the proper extensions will be added
+        fileName: (format: string, entryName: string) => {
+          if (format === 'es') return `${entryName}.js`
+          if (format === 'cjs') return `${entryName}.cjs`
+        }
+      },
+      ...(process.env.npm_lifecycle_event === 'build:library-watch' && buildToPlatformNodeModules())
+    }
+  })
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    'process.env.name': JSON.stringify(packageJson.name),
-    'process.env.version': JSON.stringify(packageJson.version),
-    'process.env.commitHEAD': JSON.stringify(execSync('git rev-parse HEAD').toString()),
-  },
-  plugins: [
-    svelte(),
-    svelteSVGPlugin()
-  ],
-  resolve: {
-    alias: {
-      $src: resolve('./src'),
-      $assets: resolve('./src/assets'),
-      $lib: resolve('./src/lib'),
-      $containers: resolve('./src/containers'),
-      $components: resolve('./src/components'),
-      $pages: resolve('./src/pages'),
-      $stores: resolve('./src/stores'),
-      $types: resolve('./src/types'),
-    }
-  },
-  ...(process.env.APP_ENV === 'library' && buildAsLibrary())
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
+ 
+  return {
+    define: {
+      'process.env.name': JSON.stringify(packageJson.name),
+      'process.env.version': JSON.stringify(packageJson.version),
+      'process.env.commitHEAD': JSON.stringify(execSync('git rev-parse HEAD').toString()),
+    },
+    plugins: [
+      svelte(),
+      svelteSVGPlugin()
+    ],
+    resolve: {
+      alias: {
+        $src: resolve('./src'),
+        $assets: resolve('./src/assets'),
+        $lib: resolve('./src/lib'),
+        $containers: resolve('./src/containers'),
+        $components: resolve('./src/components'),
+        $pages: resolve('./src/pages'),
+        $stores: resolve('./src/stores'),
+        $types: resolve('./src/types'),
+      }
+    },
+    ...(process.env.APP_ENV === 'library' && buildAsLibrary())
+  }
 })
