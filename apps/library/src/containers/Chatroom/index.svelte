84f3b58b07f17b5ff64118404d158e5b4dcdf6be
blesscat <blesscat@gmail.com>
2023-07-04 10:09:02 +0800

    [chatroom] 修正merge 錯誤

--- apps/library/src/containers/Chatroom/index.svelte
+++ apps/library/src/containers/Chatroom/index.svelte
@@ -48,7 +48,7 @@
   import { EChatroomSize } from './constant'
 
   const { chatId, iid, vipLimit, frequency } = setInfo($info)
-  const { isOpen, displayType, height, size, showBetList, device, useScrollCollapse } = setEnv($env)
+  const { isOpen, displayType, height, size, showBetList, device, useScrollCollapse, animation } = setEnv($env)
   const { sportMarketSummary, selfOrdersCallback } = setOrdersInfo($ordersInfo)
 
   const subscribeStoreModule = () => {
@@ -176,12 +176,9 @@
   <Minimize {lastReadId} {chatMessages} on:click={expandChatroom} />
 {:else}
   <div
-    class={twMerge(
-      'relative flex flex-1 flex-col bg-white',
-      isWindow && isTransition && 'fixed w-full z-30 bottom-0'
-    )}
+    class={twMerge('relative flex flex-1 flex-col bg-white', isWindow && isTransition && 'fixed w-full z-30 bottom-0')}
     style:min-height={isWindow ? boxContainerHeight : '100%'}
-    style:max-height={isWindow ? !isTransition ? 'none' : boxContainerHeight : '100%'}
+    style:max-height={isWindow ? (!isTransition ? 'none' : boxContainerHeight) : '100%'}
     transition:fly|local={$animation && { y: isWindow ? 100 * $appHeight - $height : '100%', duration: 500 }}
     on:introend={() => {
       isTransition = false
@@ -212,6 +209,6 @@
 
     <BetListSheet bind:open={$showBetList} />
 
-    <div class='absolute inset-0 bg-white z-[-1]'></div>
+    <div class="absolute inset-0 bg-white z-[-1]" />
   </div>
 {/if}
