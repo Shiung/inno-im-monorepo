76e6b9baaa69592c803f56c0a45664e2d09bd34e
FE-Jack <jacks@innotech.me>
2023-07-10 14:58:00 +0800

    [GDIM-139, GDIM-140][BetListSheet] 1. reset data state when switch tab 2. add promise callback function

--- apps/library/src/pages/test/chatroom/index.svelte
+++ apps/library/src/pages/test/chatroom/index.svelte
@@ -24,7 +24,7 @@
   let isTransition: boolean = false
   let sportMarketSummary: ISportMarketSummary
   let selfOrdersCallback: () => Promise<any>
-  let followOrdersCallback: (data) => void
+  let followOrdersCallback: (data) => Promise<any>
 
   let user: IUser = { account: '', token: '' }
 
@@ -97,7 +97,12 @@
     return res.data.list
   }
 
-  const followOrders = (data) => {
+  const followOrders = async (data) => {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true)
+      }, 1000)
+    })
     console.log(data, '這裡是跟單資料')
   }
 
