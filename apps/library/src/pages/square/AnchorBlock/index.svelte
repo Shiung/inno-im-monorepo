5a6ed4aaf0a7a776bafeff24bd6dfde9d168d650
FE3-Sean <sean@innotech.me>
2023-07-22 14:36:58 +0800

    [IN-2581][square][anchorBlock]: square anchor

--- apps/library/src/pages/square/AnchorBlock/index.svelte
+++ apps/library/src/pages/square/AnchorBlock/index.svelte
@@ -1,67 +1,95 @@
-<script lang='ts'>
-import { im } from 'api'
-import { Ripple } from 'ui'
-import { twMerge } from 'tailwind-merge'
-import { push, params } from 'svelte-spa-router'
-import { createEventDispatcher } from 'svelte'
-import type { IWebAnchor } from 'api/im/types'
+<script lang="ts">
+  import { im } from 'api'
+  import { Ripple } from 'ui'
+  import { twMerge } from 'tailwind-merge'
+  import { push, params } from 'svelte-spa-router'
+  import { createEventDispatcher } from 'svelte'
+  import type { IWebAnchor } from 'api/im/types'
 
-import { t, locale } from '$stores'
-import Empty from '$containers/Empty'
-import convertSid from 'utils/convertSid'
+  import { t, locale, getUseLang } from '$stores'
+  import Empty from '$containers/Empty'
+  import { convertSid, AbortControllers } from 'utils'
 
-import Anchor from './Anchor'
-import Loading from './Loading.svelte'
-import Arrow from './images/arrow_right_small.svg'
+  import Anchor from './Anchor'
+  import Loading from './Loading.svelte'
+  import Arrow from './images/arrow_right_small.svg'
 
-import bg0 from './images/bg_style1_0.webp'
-import bg1 from './images/bg_style1_1.webp'
-import bg2 from './images/bg_style1_2.webp'
-import bg3 from './images/bg_style1_3.webp'
+  import bg0 from './images/bg_style1_0.webp'
+  import bg1 from './images/bg_style1_1.webp'
+  import bg2 from './images/bg_style1_2.webp'
+  import bg3 from './images/bg_style1_3.webp'
 
-let anchorsPromise: ReturnType<typeof im.webAnchors>
-const anchorBgs = [ bg0, bg1, bg2, bg3 ]
+  let data: Awaited<ReturnType<typeof im.webAnchors>>['data']['list'] = []
+  const anchorBgs = [bg0, bg1, bg2, bg3]
+  const abortControllers = new AbortControllers()
+  let loading: boolean = false
 
-$: {
-  if ($params?.sid && $params.sid !== '0') anchorsPromise = im.webAnchors({
-    query: { sid: convertSid($params?.sid), pageIdx: 1, pageSize: 4 },
-    headers: { 'Accept-Language': $locale }
-  })
-}
+  const fetchAnchors = async (sid: number) => {
+    data = []
 
-const dispatch = createEventDispatcher()
-const onAnchorClick = (anchor: IWebAnchor) => {
-  dispatch('change', anchor)
-}
+    abortControllers.abortControllers('fetch-anchors')
+    const controller = {
+      ctl: new AbortController(),
+      isAborted: false,
+      key: 'fetch-anchors'
+    }
+    abortControllers.addController(controller)
 
+    try {
+      loading = true
+      const response = await im.webAnchors(
+        {
+          query: { sid, lang: useLang, pageIdx: 1, pageSize: 4 },
+          headers: { 'Accept-Language': $locale }
+        },
+        { signal: controller.ctl.signal }
+      )
+
+      const { list } = response?.data || {}
+      if (list?.length) data = list
+    } catch (error) {
+      console.error(error)
+    } finally {
+      if (!controller.isAborted) loading = false
+
+      abortControllers.spliceController(controller)
+    }
+  }
+
+  const init = (sid: string, ...rest) => {
+    if (sid && sid !== '0') fetchAnchors(convertSid(sid))
+  }
+
+  $: useLang = $getUseLang()
+
+  $: init($params?.sid, useLang)
+
+  const dispatch = createEventDispatcher()
+
+  const onAnchorClick = (anchor: IWebAnchor) => {
+    dispatch('change', anchor)
+  }
 </script>
 
-<div data-cid='AnchorList' class={twMerge('bg-white rounded-[20px] pt-[16px]', $$props.class)}>
-  <div class='px-[16px] flex items-center justify-between'>
-    <div class='text-[18px] font-semibold'> {$t('anchor.finding')} </div>
+<div data-cid="AnchorList" class={twMerge('bg-white rounded-[20px] pt-[16px]', $$props.class)}>
+  <div class="px-[16px] flex items-center justify-between">
+    <div class="text-[18px] font-semibold">{$t('anchor.finding')}</div>
 
-    <Ripple class='flex items-center space-x-[6px] text-[14px] pl-2 rounded-full'
-      on:click={() => push(`/anchor/${$params.sid}`)}
-    >
+    <Ripple class="flex items-center space-x-[6px] text-[14px] pl-2 rounded-full" on:click={() => push(`/anchor/${$params.sid}`)}>
       <span> {$t('anchor.all')} </span>
-      <Arrow width={12} height={12} fill='#333333' />
+      <Arrow width={12} height={12} fill="#333333" />
     </Ripple>
   </div>
 
-  {#await anchorsPromise}
+  {#if loading}
     <Loading />
-  {:then anchors}
-
-    {#if anchors?.data?.list?.length === 0}
-      <Empty class='h-[320px]' />
-
-    {:else}
-      <div class='grid grid-cols-2 gap-[12px] p-[16px]'>
-        {#each anchors?.data?.list || [] as anchor, idx}
-          <Anchor anchor={anchor} bg={anchorBgs[idx % anchorBgs.length]} on:click={() => onAnchorClick(anchor)} />
-        {/each}
-      </div>
-    {/if}
-
-  {/await}
+  {:else if data.length === 0}
+    <Empty class="h-[320px]" />
+  {:else}
+    <div class="grid grid-cols-2 gap-[12px] p-[16px]">
+      {#each data || [] as anchor, idx}
+        <Anchor {anchor} bg={anchorBgs[idx % anchorBgs.length]} on:click={() => onAnchorClick(anchor)} />
+      {/each}
+    </div>
+  {/if}
 </div>
