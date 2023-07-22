96d4d7b091657b8202b69fc42a4b34b719f67dc0
FE3-Sean <sean@innotech.me>
2023-07-22 13:05:21 +0800

    [IN-2581][language]: refactor

--- apps/library/src/stores/language.ts
+++ apps/library/src/stores/language.ts
@@ -6,37 +6,37 @@ type AdminLangInfo = {
   defaultLang: string
   langs: { [key: string]: boolean }
 }
-type LangMap = { [key: string]: string }
-
-export const adminLangInfo = writable<AdminLangInfo>({ defaultLang: '', langs: {} })
-export const langMap = writable<LangMap>({})
-
+const initAdminLangInfo = { defaultLang: '', langs: {} }
+export const adminLangInfo = writable<AdminLangInfo>(initAdminLangInfo)
 const fetchAdminLangList = async () => {
   try {
     const response = await im.webAnchorLanguage()
     if (response.data) {
-      const map = response.data.lang.reduce((map, lang) => Object.assign(map, { [lang.code]: true }), {})
+      const map = response.data?.lang?.reduce((map, lang) => Object.assign(map, { [lang.code]: true }), {}) || {}
       adminLangInfo.set({
-        defaultLang: response.data.defaultLang,
+        defaultLang: response.data?.defaultLang || '',
         langs: map
       })
     }
   } catch (error) {
     console.error(error)
-    adminLangInfo.set(null)
+    adminLangInfo.set(initAdminLangInfo)
   }
 }
 
+type LangMap = { [key: string]: string }
+const initLangMap = {}
+export const langMap = writable<LangMap>(initLangMap)
 const fetchPlatformLangMap = async () => {
   try {
     const response = await im.webAnchorLanguageConstants()
     if (response?.data?.lang?.length) {
-      const map = response.data.lang.reduce((map, lang) => Object.assign(map, { [lang.code]: lang.thirdCode }), {})
+      const map = response.data?.lang?.reduce((map, lang) => Object.assign(map, { [lang.code]: lang.thirdCode }), {}) || {}
       langMap.set(map)
     }
   } catch (error) {
     console.error(error)
-    langMap.set(null)
+    langMap.set(initLangMap)
   }
 }
 
@@ -45,25 +45,31 @@ export const fetchLangInfo = () => {
   fetchPlatformLangMap()
 }
 
+const NO_LANG = 'NO-LANG'
 type GetUseLang = () => string
 export const getUseLang = writable<GetUseLang>(() => {
   const { langs, defaultLang } = get(adminLangInfo) || {}
+  const useLangMap = get(langMap) || {}
+  const userLocale = get(locale)
+
   if (langs && defaultLang) {
-    return langs[get(locale)] ? get(langMap)[get(locale)] : defaultLang
+    return langs[useLangMap[userLocale]] ? useLangMap[userLocale] : defaultLang
   }
 
-  return get(locale)
+  return NO_LANG
 })
 const triggerGetUseLang = () => getUseLang.update(func => func)
 locale.subscribe(triggerGetUseLang)
 adminLangInfo.subscribe(triggerGetUseLang)
 langMap.subscribe(triggerGetUseLang)
 
-type FilterByUseLang = <T extends { languageType: string }>(array: T[]) => T[]
-export const filterByUseLang = writable<FilterByUseLang>((array) => {
+type FilterListByLang = <T extends { languageType: string }>(array: T[]) => T[]
+export const filterListByLang = writable<FilterListByLang>((array) => {
   const useLang = get(getUseLang)()
+  console.log('⛔️⛔️⛔️⛔️⛔️ filterListByLang useLang', useLang)
+  if(useLang === NO_LANG) return []
 
   return array.filter(item => item.languageType === useLang)
 })
-const triggerFilterByUseLang = () => filterByUseLang.update(func => func)
-getUseLang.subscribe(triggerFilterByUseLang)
+const triggerFilterListByLang = () => filterListByLang.update(func => func)
+getUseLang.subscribe(triggerFilterListByLang)
