fb76b03bd3dd8af5cf92612c5699c0e7bb87ad06
FE3-Sean <sean@innotech.me>
2023-07-24 14:24:37 +0800

    [IN-1763]: add default image for vd007

--- apps/library/src/components/VendorLogo/index.svelte
+++ apps/library/src/components/VendorLogo/index.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
   import { getConfig } from 'env-config'
   import { twMerge } from 'tailwind-merge'
+  import { locale } from '$stores'
 
+  const DEFAULT_RESOURCE = `${getConfig().FE_CDN_URL}/frontend/${getConfig().DEPLOY_ENV}/fe-images/${
+    getConfig().VENDERID
+  }/logo/primary_logo.png`
   let error: boolean = false
-  export let src = `${getConfig().FE_CDN_URL}/frontend/${getConfig().DEPLOY_ENV}/fe-images/${getConfig().VENDERID}/logo/primary_logo.png`
+  export let src = DEFAULT_RESOURCE
+
+  const getVdSource = (locale) => {
+    if (src !== DEFAULT_RESOURCE) return src
+
+    if (getConfig().vendor_id !== 7) return src
+
+    return ['zh_CN', 'zh_HK'].includes(locale)
+      ? `${getConfig().FE_CDN_URL}/frontend/${getConfig().DEPLOY_ENV}/fe-images/${getConfig().VENDERID}/logo/primary_zh_logo.png`
+      : DEFAULT_RESOURCE
+  }
 </script>
 
 {#if !error && src}
-  <img class={twMerge('block', $$props.class)} src={src} on:error={() => error = true} alt='' />
+  <img class={twMerge('block', $$props.class)} src={getVdSource($locale)} on:error={() => (error = true)} alt="" />
 {/if}
