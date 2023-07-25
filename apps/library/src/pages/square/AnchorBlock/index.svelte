f3d2de74e4978b8db94d3e5cadc1f4d18ad40db9
FE3-Sean <sean@innotech.me>
2023-07-25 11:50:33 +0800

    [square][anchorBlock]: fix call api twice error

--- apps/library/src/pages/square/AnchorBlock/index.svelte
+++ apps/library/src/pages/square/AnchorBlock/index.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-  import { im } from 'api'
   import { Ripple } from 'ui'
   import { twMerge } from 'tailwind-merge'
   import { push, params } from 'svelte-spa-router'
   import { createEventDispatcher } from 'svelte'
   import type { IWebAnchor } from 'api/im/types'
 
-  import { t, locale, getUseLang } from '$stores'
+  import { t } from '$stores'
   import Empty from '$containers/Empty'
-  import { convertSid, AbortControllers } from 'utils'
 
   import Anchor from './Anchor'
   import Loading from './Loading.svelte'
@@ -19,50 +17,10 @@
   import bg2 from './images/bg_style1_2.webp'
   import bg3 from './images/bg_style1_3.webp'
 
-  let data: Awaited<ReturnType<typeof im.webAnchors>>['data']['list'] = []
+  export let data: IWebAnchor[] = []
+  export let loading: boolean = false
+
   const anchorBgs = [bg0, bg1, bg2, bg3]
-  const abortControllers = new AbortControllers()
-  let loading: boolean = false
-
-  const fetchAnchors = async (sid: number) => {
-    data = []
-
-    abortControllers.abortControllers('fetch-anchors')
-    const controller = {
-      ctl: new AbortController(),
-      isAborted: false,
-      key: 'fetch-anchors'
-    }
-    abortControllers.addController(controller)
-
-    try {
-      loading = true
-      const response = await im.webAnchors(
-        {
-          query: { sid, lang: useLang, pageIdx: 1, pageSize: 4 },
-          headers: { 'Accept-Language': $locale }
-        },
-        { signal: controller.ctl.signal }
-      )
-
-      const { list } = response?.data || {}
-      if (list?.length) data = list
-    } catch (error) {
-      console.error(error)
-    } finally {
-      if (!controller.isAborted) loading = false
-
-      abortControllers.spliceController(controller)
-    }
-  }
-
-  const init = (sid: string, ...rest) => {
-    if (sid && sid !== '0') fetchAnchors(convertSid(sid))
-  }
-
-  $: useLang = $getUseLang()
-
-  $: init($params?.sid, useLang)
 
   const dispatch = createEventDispatcher()
 
