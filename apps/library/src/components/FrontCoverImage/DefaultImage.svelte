466e46c09eb691413ec72b0fec56a451d755e103
FE-Jack <jacks@innotech.me>
2023-07-25 11:10:27 +0800

    [fix][platform][anchors] fix anchors house image width in 110px & default image index

--- apps/library/src/components/FrontCoverImage/DefaultImage.svelte
+++ apps/library/src/components/FrontCoverImage/DefaultImage.svelte
@@ -6,5 +6,5 @@
 
 <div class="relative h-full">
   <img class={twMerge('max-w-none',$$props.class)} src={background} alt="">
-  <img class="absolute bottom-[2.5px] left-1/2 -translate-x-1/2 z-10 w-1/2" src={person} alt="">
+  <img class="absolute bottom-[2.5px] left-1/2 -translate-x-1/2 w-1/2" src={person} alt="">
 </div>
