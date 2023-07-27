342935f65508e1b06dcaa7d4320849351c7e5ab2
blesscat <blesscat@gmail.com>
2023-07-27 15:23:24 +0800

    [GDIN-173] 專家相關加上lottie動畫

---
+++ apps/library/src/containers/BottomNavigation/SunGlass.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { LottiePlayer } from '@lottiefiles/svelte-lottie-player'
+  import sandGlassData from './json/sandglass.json'
+</script>
+
+<LottiePlayer src={JSON.stringify(sandGlassData)} autoplay loop width={20} height={20} />
