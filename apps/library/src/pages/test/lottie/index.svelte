d03a6cbadc73873438892662cb94c5e89d211cf6
FE3-Sean <sean@innotech.me>
2023-07-28 11:57:22 +0800

    remove unused code

--- apps/library/src/pages/test/lottie/index.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import json from './data/sandglass.json'
-  import { LottiePlayer } from '@lottiefiles/svelte-lottie-player'
-</script>
-
-<div>lottie</div>
-
-<LottiePlayer src={JSON.stringify(json)} autoplay={true} loop={true} renderer="svg" height={20} width={20} />
