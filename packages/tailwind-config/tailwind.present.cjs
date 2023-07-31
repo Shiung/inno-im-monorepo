2b633261517c719b1556552f8e56ed1d878f3047
FE3-Sean <sean@innotech.me>
2023-07-31 14:55:24 +0800

    [tailwind.present]: create imsecondary vars

--- packages/tailwind-config/tailwind.present.cjs
+++ packages/tailwind-config/tailwind.present.cjs
@@ -6,6 +6,9 @@ module.exports = {
           light: 'rgba(var(--im-monorepo-secondary))',
           DEFAULT: 'rgb(var(--im-monorepo-primary))'
         },
+        imsecondary: {
+          DEFAULT: 'rgba(var(--im-monorepo-secondary))'
+        }
       },
     }
   }
