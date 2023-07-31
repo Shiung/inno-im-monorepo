eedbb3139a69f1e20fd6d65b4c7b180cf9bc933b
FE-David <davidj@innotech.me>
2023-07-31 15:08:34 +0800

    [IN-1542][FloatKey] Revert background gradient

--- apps/library/src/containers/FloatKey/Minimize/index.svelte
+++ apps/library/src/containers/FloatKey/Minimize/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { twMerge } from 'tailwind-merge'
+
   import Number from '../Number/index.svelte'
   import Key from '../images/key.svg'
 
@@ -7,7 +9,12 @@
 </script>
 
 <div>
-  <div class="w-full h-full rounded-[16px] shadow-[0_2px_2px_0_rgba(51,51,51,0.4)] bg-imprimary">
+  <div
+    class={twMerge(
+      'w-full h-full rounded-[16px] shadow-[0_2px_2px_0_rgba(51,51,51,0.4)]',
+      'bg-gradient-to-b from-[rgb(var(--im-monorepo-primary))] to-[rgb(var(--im-monorepo-secondary))]'
+    )}
+  >
     <Key width={52} height={52} />
   </div>
   <Number {remainCount} {totalCount} />
