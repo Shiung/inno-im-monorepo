51779587656826ef8abf2ec9c4d52a3e3560d1b5
FE3-Sean <sean@innotech.me>
2023-07-31 15:50:35 +0800

    [VendorLogo]: add type

--- apps/library/src/components/VendorLogo/index.svelte
+++ apps/library/src/components/VendorLogo/index.svelte
@@ -2,6 +2,7 @@
   import { getConfig } from 'env-config'
   import { twMerge } from 'tailwind-merge'
   import { locale } from '$stores'
+  import type { ILanguages } from 'env-config'
 
   const DEFAULT_RESOURCE = `${getConfig().FE_CDN_URL}/frontend/${getConfig().DEPLOY_ENV}/fe-images/${
     getConfig().VENDERID
@@ -9,7 +10,7 @@
   let error: boolean = false
   export let src = DEFAULT_RESOURCE
 
-  const getVdSource = (locale) => {
+  const getVdSource = (locale: ILanguages) => {
     if (src !== DEFAULT_RESOURCE) return src
 
     if (getConfig().vendor_id !== 7) return src
