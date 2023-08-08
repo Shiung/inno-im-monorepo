55f20aaf7713141d1439aa5c5a7b0e6960009f64
FE-Xin <xin.liao@innotech.me>
2023-08-08 11:54:53 +0800

    [IN-3707][VideoControls] 新增影片控制器

--- apps/library/src/containers/InStreamingPlayer/index.svelte
+++ apps/library/src/containers/InStreamingPlayer/index.svelte
@@ -5,9 +5,11 @@
 </script>
 
 <script lang="ts">
+  import type { SvelteComponent } from 'svelte'
   import FlvPlayer from 'ui/components/FlvPlayer'
   import VideoPlayer from 'ui/components/VideoPlayer'
   import HouseImage from '$containers/AnchorHouseImage'
+  import VideoControls from '$containers/VideoControls/index.svelte'
 
   import { StreamLiveStatus } from '$src/constant'
   import type { TStreamLiveStatus } from '$src/types'
@@ -15,8 +17,6 @@
   export let onReadyCallback = () => {}
   export let onLostDataCallback = () => {}
   export let onErrorCallback = () => {}
-  export let onPausedCallback = () => {}
-  export let onMutedCallback = () => {}
   export let streaming: {
     playStreamAddress: string;
     playStreamAddress2: string;
@@ -25,31 +25,62 @@
   }
   export let useDefControls: boolean = false
 
+  let showControls = false
+  let muted = false
+  let paused = false
+  let dom: SvelteComponent
+
+  const onMutedCallback = (m: boolean) => {
+    muted = m
+  }
+
+  const onPausedCallback = (p: boolean) => {
+    paused = p
+  }
 </script>
 
-{#if streaming?.liveStatus === StreamLiveStatus.LIVE}
-  {#if isFlvUse}
-    <FlvPlayer
-      url={streaming?.playStreamAddress}
-      controls={useDefControls}
-      internal
-      internalOnReadyCallback={onReadyCallback}
-      internalOnLostDataCallback={onLostDataCallback}
-      internalOnErrorCallback={onErrorCallback}
-      internalOnPausedCallback={onPausedCallback}
-      internalOnMutedCallback={onMutedCallback}
-    />
+<div 
+  on:click={() => {
+  if (useDefControls) showControls = true
+  }} 
+  on:keypress
+  class="relative"
+>
+  {#if streaming?.liveStatus === StreamLiveStatus.LIVE}
+    {#if isFlvUse}
+      <FlvPlayer
+        bind:this={dom}
+        url={streaming?.playStreamAddress}
+        internal
+        internalOnReadyCallback={onReadyCallback}
+        internalOnLostDataCallback={onLostDataCallback}
+        internalOnErrorCallback={onErrorCallback}
+        internalOnPausedCallback={onPausedCallback}
+        internalOnMutedCallback={onMutedCallback}
+      />
+    {:else}
+      <VideoPlayer
+        bind:this={dom}
+        urlm3u8={streaming?.playStreamAddress2}
+        internal
+        internalOnReadyCallback={onReadyCallback}
+        internalOnErrorCallback={onErrorCallback}
+        internalOnPausedCallback={onPausedCallback}
+        internalOnMutedCallback={onMutedCallback}
+      />
+    {/if}
+
+    {#if useDefControls}
+      <VideoControls 
+        bind:showControls
+        bind:paused
+        bind:muted
+        setPause={() => dom?.inPauseHandler(true)}
+        setMute={dom?.inMuteHandler}
+        setFullScreen={dom?.inFullScreenHandler}
+      />
+    {/if}
   {:else}
-    <VideoPlayer
-      urlm3u8={streaming?.playStreamAddress2}
-      controls={useDefControls}
-      internal
-      internalOnReadyCallback={onReadyCallback}
-      internalOnErrorCallback={onErrorCallback}
-      internalOnPausedCallback={onPausedCallback}
-      internalOnMutedCallback={onMutedCallback}
-    />
+    <HouseImage src={streaming?.houseImage} />
   {/if}
-{:else}
-  <HouseImage src={streaming?.houseImage} />
-{/if}
+</div>
