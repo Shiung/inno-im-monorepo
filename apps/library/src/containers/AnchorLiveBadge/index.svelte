ec52d602eb5c8cde3dc510be1a136fd5f00b786f
FE3-Sean <sean@innotech.me>
2023-08-08 23:25:37 +0800

    [AnchorLiveBadge]: change live icon to lottie

--- apps/library/src/containers/AnchorLiveBadge/index.svelte
+++ apps/library/src/containers/AnchorLiveBadge/index.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
   import { Badget } from 'ui'
+  import { LottiePlayer } from '@lottiefiles/svelte-lottie-player'
+  import LiveMotion from './lottie/live_motion.json'
 </script>
 
 <Badget
   class='flex items-center rounded-tl-[8.5px] rounded-br-[8.5px] space-x-1'
   background='linear-gradient(270deg, #FF508E 0%, #EA4C4C 100%)'
 >
-  <div class='flex items-end space-x-[1px]'>
-    <div class='h-[2px] border border-[#fff] border-t-0 border-b-0 border-r-0'></div>
-    <div class='h-[5px] border border-[#fff] border-t-0 border-b-0 border-r-0'></div>
-    <div class='h-[3px] border border-[#fff] border-t-0 border-b-0 border-r-0'></div>
-  </div>
+  <LottiePlayer src={JSON.stringify(LiveMotion)} width={6} height={6} loop autoplay />
   <span>LIVE</span>
 </Badget>
