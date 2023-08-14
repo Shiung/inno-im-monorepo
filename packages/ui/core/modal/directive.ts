77148428b8865052b60e85475eb580a45ca2bfb6
Sean Zhengg <s410585038@gmail.com>
2023-08-14 23:46:03 +0800

    [modal]: add comments

--- packages/ui/core/modal/directive.ts
+++ packages/ui/core/modal/directive.ts
@@ -7,6 +7,11 @@ export const portal = (node: HTMLElement) => {
 
   return {
     destroy() {
+      // code : packages/ui/core/modal/components/base/base.svelte
+      // 因為多包了一層 div 在 modal 在 {#if show} 外面
+      // 會導致 svelte 在 destroy 被 portal 的元素時沒有正常地從 container 刪除那個元素
+      // 使得其留在畫面上，所以必須手動判斷與刪除
+      // details: https://github.com/sveltejs/svelte/issues/4036#issuecomment-716341739
       removePortalElementWhenDestroyed({ node })
       .then(() => removeModalRootIfEmpty())
     }
