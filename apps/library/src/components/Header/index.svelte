0ba435d24627f596f0b09440083db7328488622a
FE3-Sean <sean@innotech.me>
2023-08-15 11:53:24 +0800

    [Header]: style change

--- apps/library/src/components/Header/index.svelte
+++ apps/library/src/components/Header/index.svelte
@@ -4,10 +4,10 @@
 
 </script>
 
-<div class={twMerge('flex justify-between items-center bg-white im-shadow rounded-b-[20px] h-[44px]', $$props.class)}>
+<div class={twMerge('flex justify-between items-center bg-white h-[44px]', $$props.class)}>
   <div>
     <slot name='left'>
-      <VendorLogo class='pl-3 h-[26px] w-auto' />
+      <VendorLogo class='h-[26px] w-auto' />
     </slot>
   </div>
 
