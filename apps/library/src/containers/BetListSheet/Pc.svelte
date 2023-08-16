e242110c6c3c8eba8d453088adbc1e69fbfe9424
FE3-Sean <sean@innotech.me>
2023-08-16 12:11:10 +0800

    [Dialog]: add absolute by default

--- apps/library/src/containers/BetListSheet/Pc.svelte
+++ apps/library/src/containers/BetListSheet/Pc.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import Dialog from 'ui/components/Dialog/index.svelte'
-  import { isXl } from '$stores'
   export let open: boolean
 
   const onDismiss = () => (open = false)
@@ -10,7 +9,7 @@
   class={'rounded-[10px] w-[95%] bottom-[75px] left-[2.5%]'}
   bind:open
   initHeight={(height) => height * 0.5}
-  absolute={$isXl}
+  absolute
   {onDismiss}
 >
   <slot />
