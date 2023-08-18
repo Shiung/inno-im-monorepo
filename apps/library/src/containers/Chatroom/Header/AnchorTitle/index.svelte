939a1000dc917bbe94711107e299d58cb1c0e358
FE3-Sean <sean@innotech.me>
2023-08-18 14:23:08 +0800

    [Chatroom][Header]: add AnchorTitle comp

---
+++ apps/library/src/containers/Chatroom/Header/AnchorTitle/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Ripple } from 'ui'
+  import type { IPlatformAnchor } from '$src/platform/anchors/types'
+  import AnchorImage from '$containers/AnchorImage'
+  import AnchorDetailSheet from '$containers/AnchorDetailSheet'
+  import { t } from '$stores'
+
+  let openDetailSheet: boolean = false
+  export let anchor: IPlatformAnchor
+</script>
+
+<Ripple on:click={() => (openDetailSheet = true)} class="w-[32px] h-[32px] rounded-full p-[1px] flex-none">
+  <AnchorImage src={anchor.userImage} class={'block w-full h-full border-imprimary'} borderWidth={1} />
+</Ripple>
+
+<div class="ml-2 text-[14px] font-semibold truncate">{$t('chat.anchorTitle', { anchor: anchor?.nickName || '' })}</div>
+
+<AnchorDetailSheet bind:open={openDetailSheet} houseId={anchor.houseId} />
