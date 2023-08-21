d09dbca7abbe037f32d1916143ce20a5d8da9023
Ming AY <ming-auyeung@vgt-hk.net>
2023-08-21 18:55:20 +0800

    [IN-5969]: fix height for anchor house image

--- apps/library/src/containers/AnchorHouseImage/index.svelte
+++ apps/library/src/containers/AnchorHouseImage/index.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount } from 'svelte'
   import { twMerge } from 'tailwind-merge'
-  
   import Loading from './Loading.svelte'
 
-  import { imgHeightRatio } from './config'
-  import { debounce } from 'utils'
-
   export let src: string
-  export let defaultSrc: string = ''
+  export let defaultSrc = ''
+
   let loading = true
-  let imgHeight: number = 0
+  let containerWidth: number
+  let imgHeight = 0
 
   const onError = () => {
-    if(defaultSrc) src = defaultSrc
+    if (defaultSrc) src = defaultSrc
     loading = false
   }
 
@@ -21,35 +19,32 @@
     loading = false
   }
 
-  const setRatioHeight = () => imgHeight = imgHeightRatio()
-
-  const handleResize = debounce(setRatioHeight, 100)
-  
   onMount(() => {
     const img = new Image()
     img.src = src
     img.addEventListener('load', onLoad)
     img.addEventListener('error', onError)
 
-    setRatioHeight()
-    window.addEventListener('resize', handleResize)
+    return () => {
+      img.removeEventListener('load', onLoad)
+      img.removeEventListener('error', onError)
+    }
   })
 
-  onDestroy(() => {
-    window.removeEventListener('resize', handleResize)
-  })
-
+  $: imgHeight = containerWidth * 9 / 16
 </script>
 
-{#if loading}
-  <Loading {imgHeight } />
-{:else if src}
-  <div
-    data-cid='HouseImage'
-    class={twMerge('block bg-no-repeat bg-center bg-cover h-0', $$props.class)}
-    style:background-image={`url(${src})`}
-    style:height={`${imgHeight}px`}
-  ></div>
-{:else}
-  <div style:height={`${imgHeight}px`}></div>
-{/if}
+<div bind:clientWidth={containerWidth}>
+  {#if loading}
+    <Loading {imgHeight} />
+  {:else if src}
+    <div
+      data-cid="HouseImage"
+      class={twMerge('block bg-no-repeat bg-center bg-cover h-0', $$props.class)}
+      style:background-image={`url(${src})`}
+      style:height={`${imgHeight}px`}
+    />
+  {:else}
+    <div style:height={`${imgHeight}px`} />
+  {/if}
+</div>
