414d881cac5bfcb935c9b3bfa1a44146d38542b7
FE3-Sean <sean@innotech.me>
2023-09-01 14:55:24 +0800

    [IN-1007][BetListSheet][DetailHeader]: adjust header tabs style

--- apps/library/src/containers/BetListSheet/DetailHeader/index.svelte
+++ apps/library/src/containers/BetListSheet/DetailHeader/index.svelte
@@ -7,15 +7,15 @@
   export let tabs: string[]
 </script>
 
-<div class="flex justify-between text-[16px] lg:text-[14px]">
+<div class="flex justify-evenly text-[16px] lg:text-[14px] space-x-[15px] px-4">
   {#each tabs as tab}
     <div
-      class="flex justify-center items-center duration-300 rounded-[10px] max-w-[45%]"
+      class="flex justify-center items-center rounded-[10px] flex-1"
       style:background-color={activedTab === tab ? 'rgb(var(--im-monorepo-primary))' : '#eeeeee'}
     >
       <Ripple
         class={twMerge(
-          'text-[#333333] h-[32px] w-[164px]',
+          'text-[#333333] h-[32px] w-full',
           activedTab === tab && 'text-white font-semibold'
         )}
         ripple="#ffffff"
