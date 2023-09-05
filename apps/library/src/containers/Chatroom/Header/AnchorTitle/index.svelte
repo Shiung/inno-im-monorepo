d09d0df13ebfb622b336d1b6c36346fdd7e13ad2
FE3-Sean <sean@innotech.me>
2023-09-05 14:43:22 +0800

    [IN-8013][NormalHeader][AnchorTitle]: anchor title data get from anchorDetail api rather than platform streaming

--- apps/library/src/containers/Chatroom/Header/AnchorTitle/index.svelte
+++ apps/library/src/containers/Chatroom/Header/AnchorTitle/index.svelte
@@ -1,18 +1,50 @@
 <script lang="ts">
   import { Ripple } from 'ui'
-  import type { IPlatformAnchor } from '$src/platform/anchors/types'
+  import { im } from 'api'
+  import type { IWebAnchorDetail } from 'api/im/types'
+
   import AnchorImage from '$containers/AnchorImage'
   import AnchorDetailSheet from '$containers/AnchorDetailSheet'
-  import { t } from '$stores'
+  import { t, locale } from '$stores'
+  import { CODE_STATUS_OK } from '$src/constant'
 
+  export let anchorId: IWebAnchorDetail['query']['houseId']
+  
+  const defaultAnchor = { userImage: '', nickName: '', personalIntroduction: ''}
   let openDetailSheet: boolean = false
-  export let anchor: IPlatformAnchor
+  let loading: boolean = false
+  let anchor: IWebAnchorDetail['res']['data'] = defaultAnchor
+
+  const fetchAnchorDetail = async () => {
+    try {
+      loading = true
+      const response = await im.webAnchorDetail({
+        query: { houseId: anchorId },
+        headers: { 'Accept-Language': $locale }
+      })
+
+      if (response.code === CODE_STATUS_OK) {
+        anchor = response?.data || defaultAnchor
+      }
+    } catch (error) {
+      console.error("fetchAnchorDetail", error)
+      anchor = defaultAnchor
+    } finally {
+      loading = false
+    }
+  }
+  $: if (anchorId) fetchAnchorDetail()
 </script>
 
-<Ripple on:click={() => (openDetailSheet = true)} class="w-[32px] h-[32px] rounded-full p-[1px] flex-none">
-  <AnchorImage src={anchor.userImage} class={'block w-full h-full border-imprimary'} borderWidth={1} />
-</Ripple>
+{#if loading}
+  <div class='w-[32px] h-[32px] rounded-full p-[1px] flex-none bg-[#eee] animate-pulse'></div>
+  <div class="w-[80px] h-[20px] ml-2 rounded-[10px] bg-[#eee] animate-pulse"></div>
+{:else}
+  <Ripple on:click={() => (openDetailSheet = true)} class="w-[32px] h-[32px] rounded-full p-[1px] flex-none">
+    <AnchorImage src={anchor?.userImage} class={'block w-full h-full border-imprimary'} borderWidth={1} />
+  </Ripple>
 
-<div class="ml-2 text-[14px] font-semibold truncate">{$t('chat.anchorTitle', { anchor: anchor?.nickName || '' })}</div>
+  <div class="ml-2 text-[14px] font-semibold truncate">{$t('chat.anchorTitle', { anchor: anchor?.nickName || '' })}</div>
 
-<AnchorDetailSheet bind:open={openDetailSheet} houseId={anchor.houseId} />
+  <AnchorDetailSheet bind:open={openDetailSheet} houseId={anchorId} />
+{/if}
